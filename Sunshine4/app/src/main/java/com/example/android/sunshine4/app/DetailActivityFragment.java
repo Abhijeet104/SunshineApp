c71e30cea1b5841c6abcfec77c4f6f51268cf376
Abhijeet104 <abhijeetae@gmail.com>
2015-10-01 00:11:03 +0530

    show item which is clicked on ListItem
    show item which is clicked on ListItem
    
    Before settings activity

---
+++ Sunshine4/app/src/main/java/com/example/android/sunshine4/app/DetailActivityFragment.java
@@ -0,0 +1,30 @@
+package com.example.android.sunshine4.app;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+
+
+    }
+
+
+
+}
